5f712224df064ee85588c768cb5102c3e3d113f6
liuxin <xinliu421@gmail.com>
2017-12-12 14:44:22 +0800

    add mock test

--- src/main/java/org/leon/controller/UserController.java
+++ src/main/java/org/leon/controller/UserController.java
@@ -1,10 +1,7 @@
 package org.leon.controller;
 
 import org.leon.entity.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -21,12 +18,29 @@ public class UserController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String saveUser(@RequestParam User user) {
+    public String saveUser(@ModelAttribute User user) {
         users.put(user.getId(), user);
         return "success";
     }
 
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String updateUser(@PathVariable Long id, @ModelAttribute User user) {
+        User u = users.get(id);
+        u.setAge(user.getAge());
+        u.setName(user.getName());
+        users.put(u.getId(), u);
+        return "success";
+    }
 
+    @RequestMapping(value = "/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
+        return "success";
+    }
 }
